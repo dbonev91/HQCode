463884ea46d425c7ffab879603016194c1e617f1
Dimitar Bonev <dbonev91@users.noreply.github.com>
2015-06-04 19:48:46 +0300

    Program output

---
+++ SoftUni/Homeworks/SupplementaryExaminationPrepare/VehicleParkStart.cs
@@ -0,0 +1,12 @@
+namespace VehiclePark
+{
+    using Engine;
+
+    public class VehicleParkStart
+    {
+        public static void Main()
+        {
+            VehicleParkEngine.Instance.Start("car.AA1234BB.Mitko.4");
+        }
+    }
+}
